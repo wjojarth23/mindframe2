0aa7e4ca5d3477a50ddb668444ad3d9c725d18c8
wjojarth23 <150176606+wjojarth23@users.noreply.github.com>
2024-06-28 11:52:45 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<main>
+<div class="mindframeVertical">MINDFRAME</div><img src="src/lib/images/logo.svg" class="logo"/><button class="menu"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></button></main>
+<style>
+.mindframeVertical{writing-mode: vertical-rl;
+text-orientation: mixed;
+font-size:16vh;
+position: absolute;
+top:-12px;
+left:-36px;}
+main{background-image:url("");}
+.logo{width:100px;margin:10px;position: fixed; right:0px;}</style>
